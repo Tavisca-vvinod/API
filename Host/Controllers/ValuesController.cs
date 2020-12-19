9a723abde84e4ccb26517fee603c21861a853ac6
tavica-vvinod <vvinod@tavisca.com>
2020-12-19 14:28:33 +0530

    Add project files.

---
+++ Host/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{number}")]
+        public ActionResult<int> GetMultipleOf6(int number)
+        {
+            int answer = number * 6;
+            return answer;
+        }      
+    }
+}
