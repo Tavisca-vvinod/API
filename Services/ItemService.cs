7abf2d475e7d0b42b727eeb867df3ad9f3af3c46
Havock2926 <havock2926@gmail.com>
2020-12-22 22:56:59 +0530

    This is a commit

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -22,7 +22,7 @@ namespace Services
             //in this method, check if the item id is 0, if it is zero, then set response.status as failure and description as missing or null item id
             //if the id provided in request is invalid (if such an id is not present in the list), data layer will return null, in such case set status as failure and description as invalid item id
             //in success scenario, set reponse.status as success and return the response
-            int x = 354;
+            
             var item = ItemDataLayer.GetItemById(id);
             var response = new GetItemsResponse();
             response.Items = new List<Item>();
